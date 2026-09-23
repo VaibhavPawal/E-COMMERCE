99b81e2640728b906b639e4563733c2e333ebc4a
Vaibhav Pawal <pawalvaibhav13@gmail.com>
2026-09-23 15:07:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let cardname = document.querySelector("#carddiv h3");
+let cardprice = document.querySelector("#carddiv p");
+let cart = document.querySelector("#cart");
+let card=document.querySelector("#cardbtn");
+    card.addEventListener("click", function () {
+
+        window.open("https://www.flipkart.com/crewmax-printed-men-round-neck-white-t-shirt/p/itm66d3b4343b5a7?pid","_blank");
+        
+        
+});
+
+
+
+let shosename = document.querySelector("#shosediv h3");
+let shoseprice = document.querySelector("#shosediv p");
+let shose = document.querySelector("#shosebtn");
+
+shose.addEventListener("click", function () {
+    window.open("https://www.flipkart.com/vechlo-trendy-sport-fashion-sneaker-gym-running-shoe-men-outdoors/p/itm018e43b35b21a?pid", "_blank");
+    
+});
+
+
+
+    
+let watchname = document.querySelector("#watchdiv h3");
+let watchprice = document.querySelector("#watchdiv p");
+let watch=document.querySelector("#watchbtn");
+
+    watch.addEventListener("click",function(){
+        
+        window.open("https://www.flipkart.com/limestone-day-date-functioning-steel-strap-new-quartz-analog-watch-men/p/itm1b8c5adb5e306?pid","_blank");
+        
+});
+
+
+         
+  
+
+
+    
+let bagname = document.querySelector("#bagdiv h3");
+let bagprice = document.querySelector("#bagdiv p");
+let bag=document.querySelector("#bagbtn");
+    bag.addEventListener("click",function(){
+
+        window.open("https://www.flipkart.com/hp-blue-line-27-l-laptop-backpack/p/itm173d3cdc9de06?pid=BKPH6S48PA8BGZ8F&lid","_blank");
+        
+    
+});
+
+
